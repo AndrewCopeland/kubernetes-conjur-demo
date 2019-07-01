e1cd9cc37e1986e3c3487f124e37d411733bd26f
AndrewCopeland <andcope1995@gmail.com>
2019-07-01 00:04:53 -0400

    added shabang bash

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export CONJUR_ACCOUNT=`cat ~/.conjurrc  | awk '{print $2}' | sed -n 2p`
 export CONJUR_APPLIANCE_URL=`cat ~/.conjurrc  | awk '{print $2}' | sed -n 4p`
 export CONJUR_CERT_FILE=`cat ~/.conjurrc  | awk '{print $2}' | sed -n 5p`
