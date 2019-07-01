8f0eaa97f9e4010c21da1b0da772a4e650ba974e
AndrewCopeland <andcope1995@gmail.com>
2019-07-01 00:02:29 -0400

    added init

---
+++ init.sh
@@ -0,0 +1,5 @@
+export CONJUR_ACCOUNT=`cat ~/.conjurrc  | awk '{print $2}' | sed -n 2p`
+export CONJUR_APPLIANCE_URL=`cat ~/.conjurrc  | awk '{print $2}' | sed -n 4p`
+export CONJUR_CERT_FILE=`cat ~/.conjurrc  | awk '{print $2}' | sed -n 5p`
+export CONJUR_AUTHN_LOGIN=`cat ~/.netrc | awk '{print $2}' | sed -n 2p`
+export CONJUR_AUTHN_API_KEY=`cat ~/.netrc | awk '{print $2}' | sed -n 3p`
