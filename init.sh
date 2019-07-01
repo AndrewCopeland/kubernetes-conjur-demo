5b344fe121066d3cb5487c5209eb276cec6fe7f7
Andrew Copeland <50109276+AndrewCopeland@users.noreply.github.com>
2019-07-01 00:23:38 -0400

    certfile has double quotes

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CONJUR_ACCOUNT=`cat ~/.conjurrc.old  | awk '{print $2}' | sed -n 2p`
 export CONJUR_APPLIANCE_URL=`cat ~/.conjurrc.old  | awk '{print $2}' | sed -n 4p`
-export CONJUR_CERT_FILE=`cat ~/.conjurrc.old  | awk '{print $2}' | sed -n 5p`
+export CONJUR_CERT_FILE=`cat ~/.conjurrc.old  | awk '{print $2}' | sed -n 5p | sed 's/"//g'`
 export CONJUR_AUTHN_LOGIN=`cat ~/.netrc.old | awk '{print $2}' | sed -n 2p`
 export CONJUR_AUTHN_API_KEY=`cat ~/.netrc.old | awk '{print $2}' | sed -n 3p`
